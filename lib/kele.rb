4afd963db60b48a8007ea9192444dc7c0a48cc59
Laura Slingerland <laura.sling@gmail.com>
2017-09-02 22:25:06 -0400

    Checkpoint 2: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,18 +1,16 @@
 require 'httparty'
 
 class Kele
-	include HTTParty
+  include HTTParty
 
 	base_uri 'https://www.bloc.io/api/v1'
 
-	def initialize (email, password)
-		@auth_token = get_auth_token(email, password)
-	end
-
-	private
-	def get_auth_token(email, password)
-		response = self.class.post("/sessions", body: {email: email, password: password})
-		raise StandardError if response.code != 200
-		response["auth_token"]
-	end
+  def initialize(email, password)
+    post_response = self.class.post('/sessions', body: {
+        email: email,
+        password: password
+      })
+    @user_auth_token = post_response['auth_token']
+    raise "Invalid Email or Password. Try Again." if @user_auth_token.nil?
+  end
 end
