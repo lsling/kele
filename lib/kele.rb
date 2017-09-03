0c99bae1856d2f531852fd2cd1c9e97389693e77
Laura Slingerland <laura.sling@gmail.com>
2017-09-02 22:54:08 -0400

    Checkpoint 4: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -19,4 +19,12 @@ class Kele
 		response = self.class.get('/users/me', headers: { "authorization" => @user_auth_token })
 		JSON.parse(response.body)
 	end
+
+	def get_mentor_availability(mentor_id)
+      @mentor_id = 2299843
+      response = self.class.get('/mentors/2299843/student_availability', headers: { "authorization" => @user_auth_token })
+      JSON.parse(response.body)
+  end
 end
+
+# 2299843
