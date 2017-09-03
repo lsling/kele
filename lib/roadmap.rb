713c932aae3a2efabad465e57024633bf72e19a4
Laura Slingerland <laura.sling@gmail.com>
2017-09-03 00:25:00 -0400

    Checkpoint 7: In Progress

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,11 +1,23 @@
 module Roadmap
   def get_roadmap(roadmap_id)
-      response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @user_auth_token })
-      JSON.parse(response.body)
+    response = self.class.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @user_auth_token })
+    JSON.parse(response.body)
   end
 
   def get_checkpoint(checkpoint_id)
-      response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @user_auth_token })
-      JSON.parse(response.body)
+    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @user_auth_token })
+    JSON.parse(response.body)
+  end
+
+  def create_submission(checkpoint_id, enrollment_id, assignment_branch, assignment_commit_link, comment)
+    @enrollment_id = self.get_me['id']
+    response = self.class.post("/checkpoint_submissions", headers: { "authorization" => @user_auth_token },
+    body: {
+        checkpoint_id: checkpoint_id,
+        enrollment_id: @enrollment_id,
+        assignment_branch: assignment_branch,
+        assignment_commit_link: assignment_commit_link,
+        comment: comment
+    })
   end
 end
